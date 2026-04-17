4128441f953a87a2f965d8eddd1229e82096662b
unknown <arif60972@gmail.com>
2026-04-17 21:44:28 +0100

    deleted last commit

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,7 @@ import { createAuthClient } from "better-auth/react"; // make sure to import fro
 export const authClient = createAuthClient({
   //you can pass client configuration here
   // baseURL: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth`,
+  // changed this
   baseURL: process.env.NEXT_PUBLIC_APP_URL
     ? process.env.NEXT_PUBLIC_APP_URL
     : "/api/auth",
